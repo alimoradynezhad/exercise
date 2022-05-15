679deb2000bc47f5ceeaee3f9d52c0f1ea688212
alimoradynezhad <asmnbala@gmail.com>
2022-05-16 04:04:31 +0430

    add char to lst2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 list =['11','pooya','10','15.5','12','23','45','reza','65.5','65','hamid']
-lst2 =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 'x', 'w', 'y', 'z']
+lst2 =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r','s', 't', 'u', 'v', 'x', 'w', 'y', 'z']
 for x in list:
     if x[0] in lst2:
         continue
